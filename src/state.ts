1fe21159450c48d809f9c3e393506f1ec88845d5
brunoken22 <bruno_am_22@hotmail.com>
2023-02-04 13:43:13 -0300

    ACTUALIZADO

--- src/state.ts
+++ src/state.ts
@@ -1,6 +1,6 @@
 import { rtdb } from "./db";
 import map from "lodash/map";
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = process.env.BACKEND_URL || "http://localhost:3000";
 
 const state = {
    data: {
